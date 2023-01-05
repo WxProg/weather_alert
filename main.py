387346b4f00416b20558d82b13cff36167acc791
mysta <chadmysta18@gmail.com>
2023-01-04 22:54:17 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import requests
+import os
+
+api_key = os.environ["api_key"]
+OWM_ENDPOINT = "https://api.openweathermap.org/data/3.0/onecall"
+weather_parameters = {
+    "lat": 30.468580,
+    "lon": -83.410049,
+    "appid": api_key,
+    "units": "imperial",
+    "exclude": "minutely,daily,current"
+}
+
+response = requests.get(url=OWM_ENDPOINT, params=weather_parameters)
+response.raise_for_status()
+weather_data = response.json()
+hourly_forecast = weather_data["hourly"]
+forecast_12hour = hourly_forecast[:12]
+weather_conditions_12hour_codes = [hour["weather"][0]["id"] for hour in forecast_12hour]
+
+will_rain = False
+for code in weather_conditions_12hour_codes:
+    if code < 700:
+        will_rain = True
+if will_rain:
+    print("Rain")
